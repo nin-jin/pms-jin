62c058068ebe6ab9160b0e9b84ecf26f0a400f48
jin <nin-jin@ya.ru>
2014-02-23 22:57:38 +0400

    fast script loading

--- build4web/jin_build4web_js_dev.env=node.jam.js
+++ build4web/jin_build4web_js_dev.env=node.jam.js
@@ -16,32 +16,35 @@ $jin.method({ '$jin.build4web.js.dev': function( mod, vary, mods ){
     } )
     
     index.unshift( "\
-void function( modules ){                                               \n\
-    var scripts= document.getElementsByTagName( 'script' )              \n\
-    var script= document.currentScript || scripts[ scripts.length - 1 ] \n\
-    var dir= script.src.replace( /[^/]+$/, '' )                         \n\
-    try {                                                               \n\
-        document.write( '' )                                            \n\
-        var writable= true                                              \n\
-    } catch( error ){                                                   \n\
-        var writable= false                                             \n\
-    }                                                                   \n\
-    var next= function( ){                                              \n\
-        var module= modules.shift()                                     \n\
-        if( !module ) return                                            \n\
-        if( writable ) {                                                \n\
-            document.write( '<script src=\"'+dir+module+'\"></script>' )\n\
-            next()                                                      \n\
-        } else {                                                        \n\
-            var loader= document.createElement( 'script' )              \n\
-            loader.parentScript= script                                 \n\
-            loader.src= dir + module                                    \n\
-            loader.onload= next                                         \n\
-            script.parentNode.insertBefore( loader, script )            \n\
-        }                                                               \n\
-    }                                                                   \n\
-    next()                                                              \n\
-}.call( this, [                                                         \n\
+void function( modules ){                                                   \n\
+    var scripts= document.getElementsByTagName( 'script' )                  \n\
+    var script= document.currentScript || scripts[ scripts.length - 1 ]     \n\
+    var dir= script.src.replace( /[^/]+$/, '' )                             \n\
+    try {                                                                   \n\
+        document.write( '' )                                                \n\
+        var writable = true                                                 \n\
+    } catch( error ){                                                       \n\
+        var writable = false                                                \n\
+    }                                                                       \n\
+	if( writable ){                                                         \n\
+		var buffer = []                                                     \n\
+		var module; while( module = modules.shift() ){                      \n\
+			buffer.push( '<script src=\"'+dir+module+'\"></script>' )       \n\
+		}                                                                   \n\
+		document.write( buffer.join( '' ) )                                 \n\
+	} else {                                                                \n\
+		var next= function( ){                                              \n\
+			var module= modules.shift()                                     \n\
+			if( !module ) return                                            \n\
+			var loader= document.createElement( 'script' )                  \n\
+			loader.parentScript= script                                     \n\
+			loader.src= dir + module                                        \n\
+			loader.onload= next                                             \n\
+			script.parentNode.insertBefore( loader, script )                \n\
+		}                                                                   \n\
+	    next()                                                              \n\
+	}                                                                       \n\
+}.call( this, [                                                             \n\
     " )
     
     index.push( " null ])" )
