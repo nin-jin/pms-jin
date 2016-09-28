178d5fd9af928555a067b2bad9760817ae148518
nin-jin <nin-jin@ya.ru>
2016-09-28 13:28:37 +0300

    Wrap all code to autoexported $ module.

--- time/range/range_tests.stage=dev.ts
+++ time/range/range_tests.stage=dev.ts
@@ -1,14 +1,18 @@
-$mol_test( function triplets( test ){
-	test.equal(
-		$jin.time.range('2015-01-01/P1M').end.toString(),
-		'2015-02-01'
-	)
-	test.equal(
-		$jin.time.range('P1M/2015-02-01').start.toString(),
-		'2015-01-01'
-	)
-	test.equal(
-		$jin.time.range('2015-01-01/2015-02-01').duration.toString(),
-		'PT2678400S'
-	)
-})
+module $ {
+	
+	$mol_test( function triplets( test ) {
+		test.equal(
+			$jin.time.range( '2015-01-01/P1M' ).end.toString() ,
+			'2015-02-01'
+		)
+		test.equal(
+			$jin.time.range( 'P1M/2015-02-01' ).start.toString() ,
+			'2015-01-01'
+		)
+		test.equal(
+			$jin.time.range( '2015-01-01/2015-02-01' ).duration.toString() ,
+			'PT2678400S'
+		)
+	} )
+	
+}
--- type/type_tests.test.ts
+++ type/type_tests.test.ts
@@ -1,19 +1,23 @@
-$mol_test( function scalars( test ){
-	test.equal( $jin_type( void 0 ), 'Undefined' )
-	test.equal( $jin_type( null ), 'Null' )
-	test.equal( $jin_type( 0 ), 'Number' )
-	test.equal( $jin_type( '' ), 'String' )
-	test.equal( $jin_type( false ), 'Boolean' )
-})
-
-$mol_test( function common_objects( test ){
-	test.equal( $jin_type( {} ), 'Object' )
-	test.equal( $jin_type( [] ), 'Array' )
-	test.equal( $jin_type( arguments ), 'Arguments' )
-})
-
-$mol_test( function special_objects( test ){
-	test.equal( $jin_type( function(){ return this }() ), 'Global' )
-	test.equal( $jin_type( new Date ), 'Date' )
-	test.equal( $jin_type( new RegExp( '' ) ), 'RegExp' )
-})
+module $ {
+	
+	$mol_test( function scalars( test ) {
+		test.equal( $jin_type( void 0 ) , 'Undefined' )
+		test.equal( $jin_type( null ) , 'Null' )
+		test.equal( $jin_type( 0 ) , 'Number' )
+		test.equal( $jin_type( '' ) , 'String' )
+		test.equal( $jin_type( false ) , 'Boolean' )
+	} )
+	
+	$mol_test( function common_objects( test ) {
+		test.equal( $jin_type( {} ) , 'Object' )
+		test.equal( $jin_type( [] ) , 'Array' )
+		test.equal( $jin_type( arguments ) , 'Arguments' )
+	} )
+	
+	$mol_test( function special_objects( test ) {
+		test.equal( $jin_type( function() { return this }() ) , 'Global' )
+		test.equal( $jin_type( new Date ) , 'Date' )
+		test.equal( $jin_type( new RegExp( '' ) ) , 'RegExp' )
+	} )
+	
+}
